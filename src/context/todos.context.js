db0b1a3395e8942bc0cda534fe0bef2225d9c2dd
Roberto <mayanhavoc@gmail.com>
2021-11-11 12:27:32 -0800

    couldnt fix context, moving on

--- src/context/todos.context.js
+++ src/context/todos.context.js
@@ -14,8 +14,8 @@ export const DispatchContext = createContext();
 export function TodosProvider(props){
     const [todos, dispatch] = useReducer(todoReducer, defaultTodos);
     return (
-        <TodosContext.Provider value={ todos }>
-            <DispatchContext.Provider value={ dispatch }>
+        <TodosContext.Provider value={todos} >
+            <DispatchContext.Provider value={dispatch}>
                 {props.children}
             </DispatchContext.Provider>
         </TodosContext.Provider>
--- src/reducers/todo.reducers.js
+++ src/reducers/todo.reducers.js
@@ -1,9 +1,9 @@
-import { uuid } from "uuidv4";
+import { v4 as uuidv4 } from 'uuid';
 
 const reducer = (state, action) => {
     switch(action.type) {
         case "ADD":
-          return [...state, {id: uuid(), task: action.task, completed: false }];
+          return [...state, {id: uuidv4(), task: action.task, completed: false }];
         case "REMOVE":
            return state.filter(todo => todo.id !== action.id);  
         case "TOGGLE":
